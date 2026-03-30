27c404c7ab5660af61786402d72af1b66953dbe8
RobbyCaturW <robbycatur330@gmail.com>
2026-03-30 18:12:45 +0700

    Pertemuan 4_Praktikum 2.7

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,6 +4,7 @@
     <title>Data User</title>
 </head>
 <body>
+<h1>Data User</h1>
 <a href="/user/tambah">+ Tambah User</a>
 <table border="1" cellpadding="2" cellspacing="0">
     <tr>
@@ -11,6 +12,9 @@
         <th>Username</th>
         <th>Nama</th>
         <th>ID Level Pengguna</th>
+        <td>Kode Level</td>
+        <td>Nama Level</td>
+        <td>Aksi</td>
     </tr>
     @foreach ($data as $d)
     <tr>
@@ -18,6 +22,8 @@
         <td>{{ $d->username }}</td>
         <td>{{ $d->nama }}</td>
         <td>{{ $d->level_id }}</td>
+        <td>{{ $d->level->level_kode }}</td>
+        <td>{{ $d->level->level_nama }}</td>
         <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
     </tr>
     @endforeach
